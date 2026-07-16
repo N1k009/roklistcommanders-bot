d46f58eb189e839928f639dae3e57eaac87420c1
N1k009 <velizade.nicat2006@gmail.com>
2026-07-16 23:01:46 +0400

    Add files via upload

--- roklistcommander.py
+++ roklistcommander.py
@@ -9,19 +9,19 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 DATA = {
     "piyade": {
-        "alan": [("sun_tzu.png", "bai_qi.png"), ("bai_qi.png", "liu_che.png"), ("sun_tzu.png", "liu_che.png"), ("scipio.png", "liu_che.png"), ("ragnar.png", "scipio.png"), ("liu_che.png", "scipio.png"), ("liu_che.png", "alexander.png")],
-        "rally": [("scipio_aemilianus.png", "ivar.png"), ("scipio_aemilianus.png", "charles_martel.png"), ("charles_martel.png", "harald.png")],
-        "garnizon": [("gorgo.png", "tokugawa.png"), ("gorgo.png", "cheo_yeong.png"), ("gorgo.png", "heraclius.png"), ("gorgo.png", "hector.png")]
+        "alan": [("sun_tzu.png", "bai_qi.png", "piyade_alan_ekipman.png"), ("bai_qi.png", "liu_che.png", "piyade_alan_ekipman.png"), ("sun_tzu.png", "liu_che.png",  "piyade_alan_ekipman.png"), ("scipio.png", "liu_che.png", "piyade_alan_ekipman.png"), ("ragnar.png", "scipio.png", "piyade_alan_ekipman.png"), ("liu_che.png", "scipio.png", "piyade_alan_ekipman.png"), ("liu_che.png", "alexander.png", "piyade_alan_ekipman.png")],
+        "rally": [("scipio_aemilianus.png", "ivar.png", "piyade_rally_ekipman.png"), ("scipio_aemilianus.png", "charles_martel.png", "piyade_rally_ekipman.png"), ("charles_martel.png", "harald.png", "piyade_rally_ekipman.png")],
+        "garnizon": [("gorgo.png", "tokugawa.png", "piyade_garnizon_ekipman"), ("gorgo.png", "cheo_yeong.png", "piyade_garnizon_ekipman"), ("gorgo.png", "heraclius.png", "piyade_garnizon_ekipman"), ("gorgo.png", "hector.png", "piyade_garnizon_ekipman")]
     },
     "suvari": {
-        "alan": [("gang_ghamchan.png", "achilleus.png"), ("arthur.png", "achilleus.png"), ("achilleus.png", "gang_ghamchan.png"), ("arthur.png", "huo_qubing.png"), ("attila.png", "achilleus.png")],
-        "rally": [("attila.png", "subutai.png"), ("attila.png", "achilleus.png"), ("attila.png", "charles_martel.png")],
-        "garnizon": [("elanor.png", "heraclius.png")]
+        "alan": [("gang_ghamchan.png", "achilleus.png", "suvari_alan_ekipman"), ("arthur.png", "achilleus.png", "suvari_alan_ekipman"), ("achilleus.png", "gang_ghamchan.png", "suvari_alan_ekipman"), ("arthur.png", "huo_qubing.png", "suvari_alan_ekipman"), ("attila.png", "achilleus.png", "suvari_alan_ekipman")],
+        "rally": [("attila.png", "subutai.png", "suvari_rally_ekipman"), ("attila.png", "achilleus.png", "suvari_rally_ekipman"), ("attila.png", "charles_martel.png", "suvari_rally_ekipman")],
+        "garnizon": [("elanor.png", "heraclius.png", "suvari_garnizon_ekipman")]
     },
     "okcu": {
-        "alan": [("qin_shi.png", "zhuge.png"), ("alparslan.png", "hermann.png"), ("qin_shi.png", "ysg.png"), ("zhuge.png", "hermann.png")],
-        "rally": [("shapur.png", "ashurbanipal.png")],
-        "garnizon": [("hayam.png", "heraclius.png"), ("hayam.png", "cheo.png"), ("cheo_yeong.png", "heraclius.png")]
+        "alan": [("qin_shi.png", "zhuge.png", "okcu_alan_ekipman"), ("alparslan.png", "hermann.png", "okcu_alan_ekipman"), ("qin_shi.png", "ysg.png", "okcu_alan_ekipman"), ("zhuge.png", "hermann.png", "okcu_alan_ekipman")],
+        "rally": [("shapur.png", "ashurbanipal.png", "okcu_rally_ekipman")],
+        "garnizon": [("hayam.png", "heraclius.png", "okcu_garnizon_ekipman"), ("hayam.png", "cheo.png", "okcu_garnizon_ekipman"), ("cheo_yeong.png", "heraclius.png", "okcu_garnizon_ekipman")]
     },
     "mix": {
         "garnizon": [("lapu_lapu.png", "mathias.png"), ("mathias.png", "lapu_lapu.png"), ("mathias.png", "heraclius.png"), ("heraclius.png", "mathias.png")]
