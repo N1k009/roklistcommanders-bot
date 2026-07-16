5750cba4e32fefe1e7f48a50b7c53d00b975bd1a
N1k009 <velizade.nicat2006@gmail.com>
2026-07-16 23:05:05 +0400

    Add files via upload

--- roklistcommander.py
+++ roklistcommander.py
@@ -9,22 +9,22 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 DATA = {
     "piyade": {
-        "alan": [("sun_tzu.png", "bai_qi.png", "piyade_alan_ekipman.png"), ("bai_qi.png", "liu_che.png", "piyade_alan_ekipman.png"), ("sun_tzu.png", "liu_che.png",  "piyade_alan_ekipman.png"), ("scipio.png", "liu_che.png", "piyade_alan_ekipman.png"), ("ragnar.png", "scipio.png", "piyade_alan_ekipman.png"), ("liu_che.png", "scipio.png", "piyade_alan_ekipman.png"), ("liu_che.png", "alexander.png", "piyade_alan_ekipman.png")],
+        "open-field": [("sun_tzu.png", "bai_qi.png", "piyade_alan_ekipman.png"), ("bai_qi.png", "liu_che.png", "piyade_alan_ekipman.png"), ("sun_tzu.png", "liu_che.png", "piyade_alan_ekipman.png"), ("scipio.png", "liu_che.png", "piyade_alan_ekipman.png"), ("ragnar.png", "scipio.png", "piyade_alan_ekipman.png"), ("liu_che.png", "scipio.png", "piyade_alan_ekipman.png"), ("liu_che.png", "alexander.png", "piyade_alan_ekipman.png")],
         "rally": [("scipio_aemilianus.png", "ivar.png", "piyade_rally_ekipman.png"), ("scipio_aemilianus.png", "charles_martel.png", "piyade_rally_ekipman.png"), ("charles_martel.png", "harald.png", "piyade_rally_ekipman.png")],
-        "garnizon": [("gorgo.png", "tokugawa.png", "piyade_garnizon_ekipman"), ("gorgo.png", "cheo_yeong.png", "piyade_garnizon_ekipman"), ("gorgo.png", "heraclius.png", "piyade_garnizon_ekipman"), ("gorgo.png", "hector.png", "piyade_garnizon_ekipman")]
+        "garnizon": [("gorgo.png", "tokugawa.png", "piyade_garnizon_ekipman.png"), ("gorgo.png", "cheo_yeong.png", "piyade_garnizon_ekipman.png"), ("gorgo.png", "heraclius.png", "piyade_garnizon_ekipman.png"), ("gorgo.png", "hector.png", "piyade_garnizon_ekipman.png")]
     },
     "suvari": {
-        "alan": [("gang_ghamchan.png", "achilleus.png", "suvari_alan_ekipman"), ("arthur.png", "achilleus.png", "suvari_alan_ekipman"), ("achilleus.png", "gang_ghamchan.png", "suvari_alan_ekipman"), ("arthur.png", "huo_qubing.png", "suvari_alan_ekipman"), ("attila.png", "achilleus.png", "suvari_alan_ekipman")],
-        "rally": [("attila.png", "subutai.png", "suvari_rally_ekipman"), ("attila.png", "achilleus.png", "suvari_rally_ekipman"), ("attila.png", "charles_martel.png", "suvari_rally_ekipman")],
-        "garnizon": [("elanor.png", "heraclius.png", "suvari_garnizon_ekipman")]
+        "open-field": [("gang_ghamchan.png", "achilleus.png", "suvari_alan_ekipman.png"), ("arthur.png", "achilleus.png", "suvari_alan_ekipman.png"), ("achilleus.png", "gang_ghamchan.png", "suvari_alan_ekipman.png"), ("arthur.png", "huo_qubing.png", "suvari_alan_ekipman.png"), ("attila.png", "achilleus.png", "suvari_alan_ekipman.png")],
+        "rally": [("attila.png", "subutai.png", "suvari_rally_ekipman.png"), ("attila.png", "achilleus.png", "suvari_rally_ekipman.png"), ("attila.png", "charles_martel.png", "suvari_rally_ekipman.png")],
+        "garnizon": [("elanor.png", "heraclius.png", "suvari_garnizon_ekipman.png")]
     },
     "okcu": {
-        "alan": [("qin_shi.png", "zhuge.png", "okcu_alan_ekipman"), ("alparslan.png", "hermann.png", "okcu_alan_ekipman"), ("qin_shi.png", "ysg.png", "okcu_alan_ekipman"), ("zhuge.png", "hermann.png", "okcu_alan_ekipman")],
-        "rally": [("shapur.png", "ashurbanipal.png", "okcu_rally_ekipman")],
-        "garnizon": [("hayam.png", "heraclius.png", "okcu_garnizon_ekipman"), ("hayam.png", "cheo.png", "okcu_garnizon_ekipman"), ("cheo_yeong.png", "heraclius.png", "okcu_garnizon_ekipman")]
+        "open-field": [("qin_shi.png", "zhuge.png", "okcu_alan_ekipman.png"), ("alparslan.png", "hermann.png", "okcu_alan_ekipman.png"), ("qin_shi.png", "ysg.png", "okcu_alan_ekipman.png"), ("zhuge.png", "hermann.png", "okcu_alan_ekipman.png")],
+        "rally": [("shapur.png", "ashurbanipal.png", "okcu_rally_ekipman.png")],
+        "garnizon": [("hayam.png", "heraclius.png", "okcu_garnizon_ekipman.png"), ("hayam.png", "cheo.png", "okcu_garnizon_ekipman.png"), ("cheo_yeong.png", "heraclius.png", "okcu_garnizon_ekipman.png")]
     },
     "mix": {
-        "garnizon": [("lapu_lapu.png", "mathias.png"), ("mathias.png", "lapu_lapu.png"), ("mathias.png", "heraclius.png"), ("heraclius.png", "mathias.png")]
+        "garnizon": [("lapu_lapu.png", "mathias.png", "mix_garnizon_ekipman.png"), ("mathias.png", "lapu_lapu.png", "mix_garnizon_ekipman.png"), ("mathias.png", "heraclius.png", "mix_garnizon_ekipman.png"), ("heraclius.png", "mathias.png", "mix_garnizon_ekipman.png")]
     }
 }
 
@@ -39,11 +39,13 @@ class StratejiView(discord.ui.View):
         
         await interaction.response.send_message(f"**{self.kategori.upper()} - {tip.upper()} Sıralamanız:**", ephemeral=True)
         
-        for komutan1, komutan2 in kombinasyonlar:
-            mesaj = f"{komutan1.replace('.png','').replace('_',' ')} + {komutan2.replace('.png','').replace('_',' ')}"
+        for k1, k2, ekipman in kombinasyonlar:
+            mesaj = f"{k1.replace('.png','').replace('_',' ')} + {k2.replace('.png','').replace('_',' ')}"
             files = []
-            if os.path.exists(f"gorseller/{komutan1}"): files.append(discord.File(f"gorseller/{komutan1}"))
-            if os.path.exists(f"gorseller/{komutan2}"): files.append(discord.File(f"gorseller/{komutan2}"))
+            # 3'lü kontrol: k1, k2 ve ekipman
+            for dosya in [ekipman, k1, k2]:
+                if os.path.exists(f"gorseller/{dosya}"):
+                    files.append(discord.File(f"gorseller/{dosya}"))
             
             if files:
                 await interaction.followup.send(content=f"**{mesaj}**", files=files, ephemeral=True)
@@ -51,32 +53,33 @@ class StratejiView(discord.ui.View):
         if self.kategori == "mix":
             await interaction.followup.send("Liderlik komutanları genellikle 2.cil komutanlar, buff için veya garnizon için kullanılıyorlar.\nLeader commanders are generally used as secondary commanders, for buffs or for garrison.", ephemeral=True)
 
-    @discord.ui.button(label="Alan", style=discord.ButtonStyle.primary)
-    async def alan(self, i, b): await self.gonder(i, "alan")
+    @discord.ui.button(label="Open-Field", style=discord.ButtonStyle.primary)
+    async def open_field(self, i, b): await self.gonder(i, "open-field")
     @discord.ui.button(label="Rally", style=discord.ButtonStyle.danger)
     async def rally(self, i, b): await self.gonder(i, "rally")
     @discord.ui.button(label="Garnizon", style=discord.ButtonStyle.success)
     async def garnizon(self, i, b): await self.gonder(i, "garnizon")
 
+# Komutlar (Buton isimleri güncellendi)
 @bot.command()
 async def piyade(ctx): 
     await ctx.message.delete()
-    await ctx.send("Seçim yap:", view=StratejiView("piyade"))
+    await ctx.send("Piyade için seçim yap:", view=StratejiView("piyade"))
 
 @bot.command()
 async def okcu(ctx): 
     await ctx.message.delete()
-    await ctx.send("Seçim yap:", view=StratejiView("okcu"))
+    await ctx.send("Okçu için seçim yap:", view=StratejiView("okcu"))
 
 @bot.command()
 async def suvari(ctx): 
     await ctx.message.delete()
-    await ctx.send("Seçim yap:", view=StratejiView("suvari"))
+    await ctx.send("Süvari için seçim yap:", view=StratejiView("suvari"))
 
 @bot.command()
 async def mix(ctx): 
     await ctx.message.delete()
-    await ctx.send("Seçim yap:", view=StratejiView("mix"))
+    await ctx.send("Mix için seçim yap:", view=StratejiView("mix"))
 
 keep_alive()
 token = os.environ.get("DISCORD_TOKEN")
